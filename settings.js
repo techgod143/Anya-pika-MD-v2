e895014b812ba887c40ba535407a44e52ad75ac6
khatampp <106016855+khatampp@users.noreply.github.com>
2023-02-06 23:03:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,25 +4,25 @@ const chalk = require('chalk')
 
 global.botname = "Queen Anya" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.author = "Music God"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.myweb = "https://chat.whatsapp.com/LgBGbtpCz6D3mnE9VKi56V" // You can put any link 
+global.waterMark = "×͜× Music God ✓" // Your own watermark 
+global.headerText = "Music God" // text for header of menu         
+global.DocumentTitle = "music god" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
+global.ownername = "MUSIC GOD"   // Owner name //        
+global.ownernumber = ['917466008456'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : Musicgod143" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ytname = "YT: Music God"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
 global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.state = "Uttar Pradesh" // your state name
+global.district = "Rampur" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.instagramId = "MusicGod143" // Your insta ID
+global.email = "vishalbabu7895921772@gmail.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
